3efc8e9da3d79c6b300f6c720d54fdd1c59e4200
Ian Hollander <ianholla@gmail.com>
2018-06-06 17:19:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,11 +14,15 @@ class TicTacToe
     [2,5,8]  #right col
   ]
 
-  def display_board(board)
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
+  def input_to_index(user_input)
+  user_input.to_i - 1
+end
 end
